b15d959d55672c146b3f8b80ba0409f6ab9f2e2d
jing <98076350+jing2003@users.noreply.github.com>
2026-07-15 23:10:40 -1000

    feat: set up Express backend structure and health check

---
+++ server/config/reset.js
@@ -0,0 +1,9 @@
+import { pool } from "./database.js";
+import "./dotenv.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import fs from "fs";
+
+const currentPath = fileURLToPath(import.meta.url);
+
+// TODO: Issue 4 - Add database reset and seed commands
