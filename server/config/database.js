7d9e0885d54a315f04136fae4c2bfe9c70bdb6cf
jing <98076350+jing2003@users.noreply.github.com>
2026-07-22 17:02:03 -1000

    chore: add env example and configure database SSL

--- server/config/database.js
+++ server/config/database.js
@@ -5,11 +5,9 @@ const config = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
-  port: process.env.PGPORT,
+  port: Number(process.env.PGPORT || 5432),
   database: process.env.PGDATABASE,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 };
 
 export const pool = new pg.Pool(config);
