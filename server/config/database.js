fe24925aff87066b67cd931a1f43ce2939e45dd9
Abdelrahman Mohamed <abdonssir321@icloud.com>
2026-07-27 22:16:04 -0400

    refactor: streamline SSL configuration logic in database connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,16 +1,13 @@
 import pg from "pg";
 import "./dotenv.js";
 
-const sslEnabled =
-  process.env.DB_SSL === "true" || process.env.PGHOST?.includes("render.com");
-
 const config = {
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT || 5432),
   database: process.env.PGDATABASE,
-  ssl: sslEnabled ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 };
 
 export const pool = new pg.Pool(config);
