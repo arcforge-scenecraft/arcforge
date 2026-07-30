f88a612891f8235b0f0b0268426b032ad2f3eb77
AdelineG218 <47432123+AdelineG218@users.noreply.github.com>
2026-07-29 20:18:32 -0600

    fixed import statement for getCharacters

--- client/src/components/scenes/SceneForm.jsx
+++ client/src/components/scenes/SceneForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getCharacters } from "../../services/characterAPI.js";
+import { getCharacters } from "../../services/characterApi.js";
 import { getLocations } from "../../services/locationApi.js";
 
 const EMPTY_SCENE = {
